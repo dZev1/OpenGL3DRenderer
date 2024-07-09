5bacb3c1b3217354cb2056f51a8e97b1fa840c0e
dZev1 <77645025+dZev1@users.noreply.github.com>
2024-07-09 19:02:47 -0300

    Using GLM library to transform vectors and also installed stb_image

--- 3DRenderer/3DRenderer/src/main.cpp
+++ 3DRenderer/3DRenderer/src/main.cpp
@@ -1,9 +1,13 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <cmath>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -12,6 +16,25 @@ int main()
 {
 	glfwInit();
 
+	/* trying GLM
+	// translating
+	glm::vec4 v(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 transformMatrix = glm::mat4(1.0f);
+	transformMatrix = glm::translate(transformMatrix, glm::vec3(1.0f, 1.0f, 0.0f));
+	
+	v = transformMatrix * v;
+	std::cout << v.x << v.y << v.z << v.w << std::endl;
+
+	// rotating
+	float angle;
+	angle = M_PI / 2.0f;
+	transformMatrix = glm::mat4(1.0f);
+	transformMatrix = glm::rotate(transformMatrix, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+	transformMatrix = glm::scale(transformMatrix, glm::vec3(0.5f, 0.5f, 0.5f));
+	
+	v = transformMatrix * v;
+	std::cout << v.x << v.y << v.z << v.w << std::endl;
+	*/
 
 	// openGL v3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -46,7 +69,7 @@ int main()
 		processInput(window);
 
 		// rendering commands
-		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
+		glClearColor(1.0f, 0.6f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		
 		// send new frame to window
